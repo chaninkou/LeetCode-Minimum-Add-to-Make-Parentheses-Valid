a6fbb8609fd9d2723f84c11232bad0db0c3ee8bd
chaninkou <michaelkoulove@yahoo.com>
2021-05-27 23:18:24 -0700

    Redid problem for review.

--- src/leetcode921/Main.java
+++ src/leetcode921/Main.java
@@ -2,7 +2,7 @@ package leetcode921;
 
 public class Main {
 	public static void main(String[] args) {
-		String S = "()()())";
+		String S = "()()()))";
 		
 		System.out.println("Input: " + S);
 		
